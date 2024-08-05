6fbefd239d31bb591e668ead2a98792f39e7d715
yohan8560 <yohan8560@gmail.com>
2024-08-05 11:00:53 +0900

    2번째 작업 (javaFX 갠프)

--- src/main/java/com/itgroup/www/HelloController.java
+++ src/main/java/com/itgroup/www/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        // 오늘은 날씨가 너무 너무 더웡
+        // 오늘은 날씨가 너무 너무 더웡진짜 로
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
